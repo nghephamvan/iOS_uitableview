2cfc55ad1539ac1d7032b35a5b5c8d279723b17e
Nghe Pham <ilovetayninh@gmail.com>
2017-04-14 11:25:15 +0700

    [Add]table view demo

--- tableviewdemo/tableviewdemo/ViewController.m
+++ tableviewdemo/tableviewdemo/ViewController.m
@@ -8,8 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate> {
+    NSMutableArray *imageArray;
+}
+@property (weak, nonatomic) IBOutlet UITableView *tableview;
 @end
 
 @implementation ViewController
@@ -17,6 +19,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self arraySetup];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +27,18 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void) arraySetup {
+    imageArray = [NSMutableArray arrayWithArray:@[@"1.jpeg", @"2.jpg", @"3.jpeg", @"4.jpeg"]];
+}
+
+#pragma mark - UITableView DataSource Methods
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return imageArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellId = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    return cell	;
+}
 @end
